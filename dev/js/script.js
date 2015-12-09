f8331807a793431d456f23ff38faf19814fbcf2a
Lisnayk Andrey <D:\Documents\Munspel\the_bat>
2015-12-09 19:56:00 +0200

    Template is competed

--- dev/js/script.js
+++ dev/js/script.js
@@ -4,6 +4,8 @@ function RecalcMenuLine(){
 	var window_width = $(window).width();	
 	var offset=-2000+left+($("ul.nav > li.active").width()/2);
 	$(".line-bg").css({"background-position": offset+"px 47px"});
+	var scrollHeight = $(window).scrollTop() + $(window).height();
+	
 }
 $(window).load(function(){
 	RecalcMenuLine();
